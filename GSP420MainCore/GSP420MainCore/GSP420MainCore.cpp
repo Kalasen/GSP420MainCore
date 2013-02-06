b94f60ba8356a8d352b6a0dc7d89be850ec43d23
Steven Parks <zahru.tesla@hotmail.com>
2013-02-05 22:02:23 -0500

    Most of Main Core finished
    There's some odds and ends like that the error logging could be better.
    The big thing is the StateManager, but even that's mostly stubs and
    filler using console to stand in for what will be in place once we start
    core integration.

--- GSP420MainCore/GSP420MainCore/GSP420MainCore.cpp
+++ GSP420MainCore/GSP420MainCore/GSP420MainCore.cpp
@@ -1,11 +1,27 @@
-// GSP420MainCore.cpp : Defines the entry point for the console application.
+// GSP420MainCore.cpp : Defines the entry point for the console application
 //
 
 #include "stdafx.h"
+#include "StateManager.h"
+#include <iostream> // Only being used because of reporting errors to console. Delete this line when fixed up better.
+
+using namespace std;
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, _TCHAR* argv[]) //Entry point of application
 {
+	StateManager stateManager; // Makes our StateManager class
+	try //If any error we haven't handled further in occurs, we want to properly log what the error was to a text file or such.
+	{
+		stateManager.Run();
+	}
+	catch (...)
+	{
+		//TODO: Properly log whatever occurred.
+		cout << "An error occurred, but we haven't yet figured out how to properly log it. Press enter to throw the exception to the debugger.";
+		throw;
+	}
+
 	return 0;
 }
 
